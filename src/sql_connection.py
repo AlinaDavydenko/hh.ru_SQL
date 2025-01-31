c88f8636807a4d241b70e042be9a09e4bf378477
Alina Davydenko <Alinadavydenko620@gmail.com>
2025-01-31 12:27:07 +0300

    sql3

--- src/sql_connection.py
+++ src/sql_connection.py
@@ -12,6 +12,7 @@ class SqlConnection:
     DATABASE = os.getenv('DATABASE')
     USER = os.getenv('USER')
     PASSWORD = os.getenv('PASSWORD')
+    connection: psycopg2
 
     def __init__(self, list_employers: list, list_vacancies: list):
         self.list_employers = list_employers
@@ -19,34 +20,44 @@ class SqlConnection:
 
     @classmethod
     def sql_connection(cls):
-        """ подключение к бд и проектирование таблиц """
+        """ подключение к бд """
         # подключение к базе данных
-        with psycopg2.connect(
+        cls.connection = psycopg2.connect(
             host=cls.HOST,
             database=cls.DATABASE,
             user=cls.USER,
             password=cls.PASSWORD
-        ) as connection:
-            with connection.cursor() as cur:
-                # составление запросов, execute query
+        )
 
-                # таблица для работодателя
-                cur.execute('CREATE TABLE Employers('
-                            'employer_id int PRIMARY KEY, '
-                            'name VARCHAR(50),'
-                            'alternate_url VARCHAR(255),'
-                            'open_vacancies int)')
+    @classmethod
+    def build_tables(cls):
+        """ проектирование таблиц """
+        with cls.connection.cursor() as cur:
+            # составление запросов, execute query
 
-                # таблица для вакансий
-                cur.execute('CREATE TABLE Vacancies('
-                            'employer_id int PRIMARY KEY,'
-                            'vacancy_name VARCHAR(50),'
-                            'salary_from int,'
-                            'salary_to int,'
-                            'vacancy_url,'
-                            'FOREIGN KEY (employer_id) REFERENCES Employers(employer_id))')  # связь с внешним ключем id
+            # таблица для работодателя
+            cur.execute('CREATE TABLE Employers('
+                        'employer_id int PRIMARY KEY, '
+                        'name VARCHAR(50),'
+                        'alternate_url VARCHAR(255),'
+                        'open_vacancies int)')
 
-                connection.commit()
+            # таблица для вакансий
+            cur.execute('CREATE TABLE Vacancies('
+                        'employer_id int PRIMARY KEY,'
+                        'vacancy_name VARCHAR(50),'
+                        'salary_from int,'
+                        'salary_to int,'
+                        'vacancy_url,'
+                        'FOREIGN KEY (employer_id) REFERENCES Employers(employer_id))')  # связь с внешним ключем id
+
+            cls.connection.commit()
+
+    @staticmethod
+    def drop_tables():
+        cur.execute()
+
+# заполнить в таблицу данные
 
     #             rows = cur.fetchall()
     #             cur.execute('DROP TABLE vacancy')
